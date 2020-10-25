e4aa934b18ec745b0e3c1cb4fc37c98f8827d14d
Sarvjot Singh <sarvjotsingh422002@gmail.com>
2020-10-26 01:09:44 +0530

    random fixes

--- Eligibility.js
+++ Eligibility.js
@@ -1,34 +1,50 @@
-function isEligible(branch, rank, category, state_of_eligibility, seat_pool, main_advanced, college_home_states){
-    
-    var finalQuota;
-    if(category == branch.Category 
-        && seat_pool == branch.seat_pool 
-        && main_advanced == branch.iit 
-        && !(branch.branch_name.includes("Architecture"))){
-
-            // Category Matched
-            var foundQuota = false;
-            branch.quotas.forEach((quota) => {
-                if(foundQuota === false){
-                    if(quota.alloted_quota === "OS" 
-                    || quota.alloted_quota === "AI"
-                    || (quota.alloted_quota === "GO" && state_of_eligibility === "Goa")
-                    || (quota.alloted_quota === "AP" && state_of_eligibility === "Arunachal Pradesh")
-                    || (quota.alloted_quota === "HS" && college_home_states.includes(state_of_eligibility))
-                    ){
-                        // Eligiblity matched as well
-                        if((Number)(rank) <= (Number)(quota.closing_rank)){
-                            // Qualified for seat
-                            finalQuota = quota;
-                            foundQuota = true;
-                        }
-                    }
-                }
-                
-            })
+function isEligibleForStateQuota(quota, state_of_eligibility, college_home_states){
+    // if the seat has a state quota AND the candidate belongs to the same state
+    if (
+        (quota.alloted_quota === "GO" && state_of_eligibility === "Goa")
+        ||(quota.alloted_quota === "AP" && state_of_eligibility === "Arunachal Pradesh") 
+        || (quota.alloted_quota === "HS" && college_home_states.includes(state_of_eligibility))
+    ){
+        return true;
+    }else{
+        return false;
     }
-    return finalQuota;
 }
 
+function isEligibleForOtherStateQuota(quota, state_of_eligibility, college_home_states){
+    // if the seat has a other state quota AND the candidate DOES NOT belong to the same state as that of the institute
+    if(
+        (quota.alloted_quota === "OS" || quota.alloted_quota === "AI") 
+        && college_home_states.includes(state_of_eligibility) === false 
+    ){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function isEligible(branch, rank, category, state_of_eligibility, seat_pool, main_advanced, college_home_states) {
+  var finalQuota;
+  if (category == branch.Category && seat_pool == branch.seat_pool && main_advanced == branch.iit &&
+    !branch.branch_name.includes("Architecture")
+  ) {
+    // Category Matched
+    var foundQuota = false;
+    branch.quotas.forEach((quota) => {
+      if (foundQuota === false) {
+        if (
+            isEligibleForStateQuota(quota,state_of_eligibility, college_home_states)
+            || isEligibleForOtherStateQuota(quota, state_of_eligibility, college_home_states)  
+        ) {
+          if (Number(rank) <= Number(quota.closing_rank)) {
+            finalQuota = quota;
+            foundQuota = true;
+          }
+        }
+      }
+    });
+  }
+  return finalQuota;
+}
 
 module.exports = isEligible;
---
+++ index.js
@@ -0,0 +1,49 @@
+var express = require("express"),
+  app = express(),
+  mongoose = require("mongoose"),
+  bodyParser = require("body-parser"),
+  College = require("./models/college"),
+  seedDB = require("./seed"),
+  isEligible = require("./Eligibility.js"),
+  port = 3000;
+
+// Connecting mongoose to the local database
+mongoose.connect("mongodb://localhost:27017/rank_predictor", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+// seeding the database
+// seedDB();
+
+// configuring different plugins
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/colleges", (req, res) => {
+  College.find({}, (err, allColleges) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("collegeList", {
+        isEligible: isEligible,
+        colleges: allColleges,
+        rank: req.body.rank,
+        category: req.body.category,
+        seat_pool: req.body.seat_pool,
+        // alloted_quota: req.body.alloted_quota,
+        state_of_eligibility: req.body.state_of_eligibility,
+        main_advanced: req.body.main_adv
+      });
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
