6a209f99d97805bcb977a781c37684a16c6d0211
Sarvjot Singh <sarvjotsingh422002@gmail.com>
2020-11-05 13:41:54 +0530

    Crucial Changes

--- public/js/app.js
+++ public/js/app.js
@@ -2,20 +2,18 @@ var advancedBtn = document.querySelector("#advanced");
 var mainBtn = document.querySelector("#main");
 var rankInput = document.querySelector("#rank");
 var stateInput = document.querySelector("#state_of_eligibility");
-var nextButton = document.querySelector("#next");
-var inputDivs = document.querySelectorAll(".input-div");
+var inputDiv = document.querySelector("#input-div");
 
 // ADDING MAINS ADVANCED BUTTON FUNCTIONALITY
 
 advancedBtn.addEventListener("change",() => {
     stateInput.style.display = "none";
+    inputDiv.style.gridTemplateAreas = '"category-rank category-rank" "category seat-pool"';
+    rankInput.placeholder = "Enter JEE Advanced Category Rank";
 })
 mainBtn.addEventListener("change",() => {
     stateInput.style.display = "flex";
-})
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    $('.js-example-basic-single').select2();
+    inputDiv.style.gridTemplateAreas = '"category-rank category" "seat-pool state"';
+    rankInput.placeholder = "Enter JEE Mains Category Rank"
 })
 
